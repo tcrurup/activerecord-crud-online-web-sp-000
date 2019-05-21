c41f870c1b2cee7ee9efb6ce120a4ad51e8737f1
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 19:01:43 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -55,7 +55,12 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  args = {
+    :title => "Title"
+    :release_date => 2000
+    :director => "Me"
+  }
+  Movie.find()
 end
 
 def can_find_using_where_clause_and_be_sorted
